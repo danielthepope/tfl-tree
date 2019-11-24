8520dc0068751980ac324d2fe2659cfa13527a48
Dan Pope <danielthepope@users.noreply.github.com>
2019-11-24 19:27:32 +0000

    Add retry logic to the PostUpdate call

--- tfltree/raspberrypi/tweets.py
+++ tfltree/raspberrypi/tweets.py
@@ -1,4 +1,7 @@
+from time import sleep
+
 import twitter
+from twitter.error import TwitterError
 
 from tfltree import logger as log
 from tfltree.raspberrypi.config import (ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET,
@@ -14,14 +17,27 @@ def post_video(tweet_text, video_file, subtitle_file):
     if enabled:
         api = twitter.Api(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
 
-        log.info('Uploading %s to Twitter', video_file)
+        log.debug('Uploading %s to Twitter', video_file)
         video_id = api.UploadMediaChunked(video_file, media_category='TweetVideo')
-        log.info('Video ID: %s', video_id)
-        log.info('Uploading %s to Twitter', subtitle_file)
+        log.debug('Video ID: %s', video_id)
+        log.debug('Uploading %s to Twitter', subtitle_file)
         subtitle_id = api.UploadMediaChunked(subtitle_file, media_category='Subtitles')
-        log.info('Subtitle ID: %s', subtitle_id)
+        log.debug('Subtitle ID: %s', subtitle_id)
         api.PostMediaSubtitlesCreate(video_id, subtitle_id, 'en', 'English')
-        log.info('Successfully associated video with subtitles')
-        api.PostUpdate(tweet_text, media=video_id)
+        log.debug('Successfully associated video with subtitles')
+        attempts = 0
+        while attempts < 15:
+            try:
+                status = api.PostUpdate(tweet_text, media=video_id)
+                log.info('Uploaded to Twitter successfully!')
+                log.debug(status)
+                return
+            except TwitterError:
+                attempts += 1
+                log.warn('Error from Twitter after %s attempts. Trying a bit later', attempts)
+                sleep(5)
+        # Exceeded the maximum attempts
+        log.error('Twitter upload exceeded the maximum number of attempts. Giving up.')
+
     else:
         log.warning('Twitter upload not enabled. Requires API tokens.')
